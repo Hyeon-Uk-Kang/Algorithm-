d2c7558a6d394f1274ba188af1bd2668015ea7e3
Hyeon-Uk-Kang <jaron0920@naver.com>
2022-05-09 01:31:49 +0900

    Add files via upload

---
+++ BOJ/백준 12015번.py
@@ -0,0 +1,14 @@
+import bisect
+import sys
+
+n=int(input())
+arr=list(map(int,sys.stdin.readline().split()))
+
+dp=[arr[0]]
+for i in range(1,n):
+    if arr[i]>dp[-1]:
+        dp.append(arr[i])
+    else:
+        low=bisect.bisect_left(dp,arr[i])
+        dp[low]=arr[i]
+print(len(dp))
