f9bb98d9772c149a8c44c1dd5b71f1ef5e4a8cc9
Hyeon-Uk-Kang <jaron0920@naver.com>
2022-08-22 10:46:50 +0900

    Add files via upload

--- BOJ/백준 2887번.py
+++ BOJ/백준 2887번.py
@@ -1,8 +1,9 @@
 import sys
 
 n=int(input())
-parent=[i for i in range(n+1)]
 x,y,z=[],[],[]
+parent=[i for i in range(n+1)]
+
 for i in range(n):
     a,b,c=map(int,sys.stdin.readline().split())
     x.append((a,i))
@@ -12,27 +13,34 @@ for i in range(n):
 x.sort()
 y.sort()
 z.sort()
+
 edge=[]
+
 for i in range(n-1):
-    edge.append((x[i+1][0]-x[i][0],x[i+1][1],x[i][1]))
-    edge.append((y[i+1][0] - y[i][0],y[i+1][1],y[i][1]))
-    edge.append((z[i+1][0] - z[i][0],z[i+1][1],z[i][1]))
+    edge.append((x[i + 1][0] - x[i][0], x[i + 1][1], x[i][1]))
+    edge.append((y[i + 1][0] - y[i][0], y[i + 1][1], y[i][1]))
+    edge.append((z[i + 1][0] - z[i][0], z[i + 1][1], z[i][1]))
 
 edge.sort()
-result=0
 
 def find(x):
     if parent[x]!=x:
         parent[x]=find(parent[x])
+
     return parent[x]
+
 def union(a,b):
     a=find(a)
     b=find(b)
+
     if a!=b:
         parent[b]=a
+
+result=0
 for i in edge:
     dist,a,b=i
     if find(a)!=find(b):
         union(a,b)
         result+=dist
+
 print(result)
