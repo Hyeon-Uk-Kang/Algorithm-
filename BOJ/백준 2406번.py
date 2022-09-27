bb5685fdf5481d035d7b0a9a94514722595e18a9
Hyeon-Uk-Kang <jaron0920@naver.com>
2022-09-28 00:11:06 +0900

    Add files via upload

--- BOJ/백준 2406번.py
+++ BOJ/백준 2406번.py
@@ -1,61 +1,53 @@
-import sys
+n,m=map(int,input().split())
 
-input = sys.stdin.readline
-import heapq
+p=[i for i in range(n+1)]
 
+def find(x):
 
-def find_parent(parent, x):
-    if x != parent[x]:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+    if x!=p[x]:
+        p[x]=find(p[x])
 
+    return p[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+def unite(a,b):
+    a=find(a)
+    b=find(b)
 
+    if b>a:
+        p[b]=a
+    elif a>b:
+        p[a]=b
 
-n, m = map(int, input().split())
-parent = [i for i in range(n)]
-cnt = 1
+for i in range(m):
+    a,b=map(int,input().split())
+    unite(a,b)
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    if find_parent(parent, a - 1) != find_parent(parent, b - 1):
-        union_parent(parent, a - 1, b - 1)
-        cnt += 1
+arr=[]
+for i in range(n):
+    arr.append(list(map(int,input().split())))
 
-costs = [list(map(int, input().split())) for _ in range(n)]
-kruskal = []
+edge=[]
 
-if cnt == n - 1:
-    print(0, 0)
+for i in range(1,n-1):
+    for j in range(i+1,n):
+        edge.append((arr[i][j],i+1,j+1))
+
+edge.sort()
+result=0
+v=[]
+for i in edge:
+    cost=i[0]
+    a=i[1]
+    b=i[2]
+
+    if find(a)!=find(b):
+        unite(a,b)
+        result+=cost
+        v.append((a,b))
+
+if len(v)==0:
+    print(0,0)
 else:
-    for i in range(1, n - 1):
-        for j in range(i + 1, n):
-            if find_parent(parent, i) != find_parent(parent, j):
-                heapq.heappush(kruskal, [costs[i][j], i, j])
-
-    connected = []
-    total = 0
-
-    while kruskal:
-        cost, x, y = heapq.heappop(kruskal)
-
-        if find_parent(parent, x) == find_parent(parent, y):
-            continue
-
-        cnt += 1
-        union_parent(parent, x, y)
-        total += cost
-        connected.append([x + 1, y + 1])
-        if cnt == n - 1:
-            break
-
-    print(total, len(connected))
-    for point in connected:
-        print(*point)
+    print(result,len(v))
+    for i in v:
+        print(i[0],i[1])
